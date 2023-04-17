d1e33926d273f25fcb9a02b6814605bd84e7d6e8
nonamugu <0908jhk@naver.com>
2023-04-18 01:03:45 +0900

    20230418 react
    블로그 생성 폼 만든 후 (title, body) json-server 설치 및 실행

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,102 +1,43 @@
-import { useState } from 'react';
+import {useState} from 'react'
 
 function App() {
-  /* 함수 사용 및 Fragment */ 
-  // import {Fragment} from 'react';
-
-  // const number = 1;
-
-  // const double = (number) => {
-  //   return number * 2;
-  // };
-  // double();
-  // return (
-  //   <> or <Fragment>
-  //     <div>{number}</div>
-  //   </> or </Fragment>
-  // );
-
-  /* Click 이벤트 */
-  // const printHello = () => {
-  //   console.log('hello')
-  // };
-
-  // printHello();
-
-  // return (
-  //   <>
-  //     <button onClick={printHello}>Submit</button>
-  //   </> 
-  // );
-
-  /* useState */
-  // 컴퍼넌트가 렌더링이 되려면 state가 업데이트가 되면 화면도 업데이트가 됨
-  // import { useState } from 'react';
-
-  // doubledNumber를 통해 업데이트 후 사용할 수 있어서 콘솔로 2가 찍힘
-
-  // console.log('render');
-  // const [number, setNumber] = useState(1);    // 상태, 함수이름
-  // const [number1, setNumber1] = useState(1);
-  // const double = () => {
-  //   const doubledNumber = number * 2;
-  //   setNumber(doubledNumber);
-  //   setNumber1(number1 * 2);
-  //   console.log(doubledNumber);
-  // };
-
-  // return (
-  //   <>
-  //     <div>{number}</div>
-  //     <div>{number1}</div>
-  //     <button onClick={double}>
-  //       Submit
-  //     </button>
-  //   </> 
-  // ); 
-  
-  /* useState(prevState) */
-  // import { useState } from 'react';
-
-  // console.log('render');
-  // const [number, setNumber] = useState(1);    // 상태, 함수이름
-  // const double = () => {
-  //   // const doubledNumber = number * 2;
-  //   setNumber((prevState) => prevState * 2);  // 한 줄이면 return 없이 사용 가능
-  //   setNumber((prevState) => {
-  //     return prevState * 2
-  //   });
-  // };
-
-  // return (
-  //   <>
-  //     <div>{number}</div>
-  //     <button onClick={double}>
-  //       Submit
-  //     </button>
-  //   </> 
-  // );
-
-  /* Bootstrap 스타일 추가 */
-  // import { useState } from 'react';
-  
-  console.log('render');
-  const [number, setNumber] = useState(1);    // 상태, 함수이름
-  const double = () => {
-    // const doubledNumber = number * 2;
-    setNumber((prevState) => prevState * 2);  // 한 줄이면 return 없이 사용 가능
-    setNumber((prevState) => {
-      return prevState * 2
-    });
+  const [title, setTitle] = useState('');  // 초기값은 없어야 하기때문에 empty
+  const [body, setBody] = useState('');  // 초기값은 없어야 하기때문에 empty
+  const onSubmit = () => {
+    console.log(title, body);
   };
-  
+
   return (
-    <>
-      <div>{number}</div>
-      <button className = "btn btn-primary" onClick={double}>
-        Submit
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input 
+          className="form-control"
+          value={title}   // empty string을 밸류로 가져옴
+          onChange={(event) => {    // 입력할 때마다 setTitle을 통해 값을 가져와서 title에 업데이트를 시켜줌
+            setTitle(event.target.value);
+          }}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea 
+          className="form-control"
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);            
+          }}
+          rows={20}
+        />
+      </div>
+      <button 
+        className="btn btn-primary"
+        onClick={onSubmit}
+      >
+        Post
       </button>
-    </> 
+    </div>
   );
   
 }
