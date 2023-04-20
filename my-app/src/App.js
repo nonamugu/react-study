54d3c034c3a978b7f3701049c24c1818a4f5625c
nonamugu <0908jhk@naver.com>
2023-04-20 18:30:25 +0900

    20230420 react
    가독성을 위해 NavBar와 BlogForm 컴포넌트로 빼서 정리

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,26 @@
-import {useState} from 'react'
-import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import BlogForm from './components/BlogForm';
+import NavBar from './components/NavBar';
 
 function App() {
-  const [title, setTitle] = useState('');  // 초기값은 없어야 하기때문에 empty
-  const [body, setBody] = useState('');  // 초기값은 없어야 하기때문에 empty
-  const onSubmit = () => {
-    axios.post('http://localhost:3001/posts', {
-      title,   // 키와 변수의 이름이 같으면 하나만 쓸 수 있음 (title: title)
-      body
-    })
-  };
-
   return (
-    <div className="container">
-      <div className="mb-3">
-        <label className="form-label">Title</label>
-        <input 
-          className="form-control"
-          value={title}   // empty string을 밸류로 가져옴
-          onChange={(event) => {    // 입력할 때마다 setTitle을 통해 값을 가져와서 title에 업데이트를 시켜줌
-            setTitle(event.target.value);
-          }}
-        />
-      </div>
-
-      <div className="mb-3">
-        <label className="form-label">Body</label>
-        <textarea 
-          className="form-control"
-          value={body}
-          onChange={(event) => {
-            setBody(event.target.value);            
-          }}
-          rows={20}
-        />
-      </div>
-      <button 
-        className="btn btn-primary"
-        onClick={onSubmit}
-      >
-        Post
-      </button>
-    </div>
+      <Router>
+        <NavBar />
+        <div className='container'>
+          <Switch>
+            <Route path="/" exact>    
+              Home Page
+            </Route>
+            <Route path="/blogs">
+              <BlogForm />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
   
 }
---
+++ my-app/src/components/BlogForm.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import axios from 'axios';
+
+const BlogForm = () => {
+  const [title, setTitle] = useState('');  // 초기값은 없어야 하기때문에 empty
+  const [body, setBody] = useState('');  // 초기값은 없어야 하기때문에 empty
+  const onSubmit = () => {
+    axios.post('http://localhost:3001/posts', {
+      title,   // 키와 변수의 이름이 같으면 하나만 쓸 수 있음 (title: title)
+      body
+    })
+  };
+
+    return (
+        <div>
+            <h1>Create a blog post</h1>
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input
+                className="form-control"
+                value={title} // empty string을 밸류로 가져옴
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                } } />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Body</label>
+              <textarea
+                className="form-control"
+                value={body}
+                onChange={(event) => {
+                  setBody(event.target.value);
+                } }
+                rows={20} />
+            </div>
+            <button
+              className="btn btn-primary"
+              onClick={onSubmit}
+            >
+              Post
+            </button>
+          </div>
+    );
+};
+
+export default BlogForm;
