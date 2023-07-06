944016f70c52ca620d6103e3a072b84a9e6c7888
nonamugu <0908jhk@naver.com>
2023-07-06 17:09:56 +0900

    20230706 React 데이터 넘기기 & Card 및 props 활용 commit

--- my-app/src/pages/ListPage.js
+++ my-app/src/pages/ListPage.js
@@ -1,6 +1,31 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Card from '../components/Card';
+
 const ListPage = () => {
+    const [posts, setPosts] = useState([]);
+ 
+    const getPosts = () => {
+        axios.get('http://localhost:3001/posts').then((res) => {
+            setPosts(res.data);        
+        })
+    }
+
+    useEffect(() => {
+        getPosts();        
+    }, []);
+
     return (
-        <div>List Page</div>
+        <div>
+            <h1>Blogs</h1>
+            {posts.map(post => {
+                return (
+                    <Card key={post.id} title={post.title} >
+                        <div>hi</div>
+                    </Card>
+                );
+            })}
+        </div>
     );
 };
 
