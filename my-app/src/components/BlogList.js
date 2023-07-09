1bd464e18c97c7bba9d55d6621be3dfda53eacae
nonamugu <0908jhk@naver.com>
2023-07-09 23:57:25 +0900

    20230709 React Pagination & useCallback 사용 commit

--- my-app/src/components/BlogList.js
+++ my-app/src/components/BlogList.js
@@ -1,23 +1,61 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Card from '../components/Card';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { bool } from 'prop-types';
-
+import Pagination from './Pagination';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom';
+import { useCallback } from 'react';
 
 const BlogList = ({ isAdmin }) => {
     const history = useHistory();
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const pageParam = params.get('page');
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [numberOfPosts, setNumberOfPosts] = useState(0);
+    const [numberOfPages, setNumberOfPages] = useState(0);
+    const limit = 1;
+ 
+    useEffect(() => {
+        setNumberOfPages(Math.ceil(numberOfPosts/limit));
+    }, [numberOfPosts]);
+
+    const onClickPageButtnon = (page) => {
+        history.push(`${location.pathname}?page=${page}`)
+    };
+
     
-    const getPosts = () => {
-        axios.get('http://localhost:3001/posts').then((res) => {
+    const getPosts = useCallback((page = 1) => {
+        let params = {
+            _page: page,
+            _limit: limit,
+            _sort: 'id',
+            _order: 'desc',
+        }
+        
+        if (!isAdmin) {
+            params = { ...params, publish: true };
+        } 
+
+        axios.get(`http://localhost:3001/posts`, {
+            params
+        }).then((res) => {
+            setNumberOfPosts(res.headers['x-total-count'])
             setPosts(res.data);        
             setLoading(false);
         })
-    }
+    }, [isAdmin]);
+
+
+    useEffect(() => {
+        console.log('1')
+        setCurrentPage(parseInt(pageParam) || 1);
+        getPosts(parseInt(pageParam) || 1);
+    }, [pageParam, getPosts]);
 
     const deleteBlog = (e, id) => {
         e.stopPropagation();
@@ -26,10 +64,6 @@ const BlogList = ({ isAdmin }) => {
             setPosts(prevPosts => prevPosts.filter(post => post.id !== id));
         });
     };
-
-    useEffect(() => {
-        getPosts();        
-    }, []);
 
     if (loading) {
         return (
@@ -41,26 +75,37 @@ const BlogList = ({ isAdmin }) => {
         return (<div>No blog posts found</div>)
     }
 
-    return posts.filter(post => {
-        return isAdmin || post.publish
-    }).map(post => {
-        return (
-            <Card 
-                key={post.id} 
-                title={post.title} 
-                onClick={() => history.push(`/blogs/${post.id}`)}
-            >
-                {isAdmin ? (<div>
-                    <button 
-                        className='btn btn-danger btn-sm'
-                        onClick={(e) => deleteBlog(e, post.id)}
-                    >                                
-                        Delete
-                    </button>
-                </div>) : null}
-            </Card>
-        );
-    })
+    const renderBlogList = () => {
+        return posts.map(post => {
+            return (
+                <Card 
+                    key={post.id} 
+                    title={post.title} 
+                    onClick={() => history.push(`/blogs/${post.id}`)}
+                >
+                    {isAdmin ? (<div>
+                        <button 
+                            className='btn btn-danger btn-sm'
+                            onClick={(e) => deleteBlog(e, post.id)}
+                        >                                
+                            Delete
+                        </button>
+                    </div>) : null}
+                </Card>
+            );
+        })
+    };
+
+    return (
+        <div>
+            {renderBlogList()}
+            {numberOfPages > 1 && <Pagination 
+                currentPage={currentPage} 
+                numberOfPages={numberOfPages}
+                onClick={onClickPageButtnon}
+            />}
+        </div>
+    )
 };
 
 BlogList.propTypes = {
